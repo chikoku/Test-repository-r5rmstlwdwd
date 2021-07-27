f914557723409901bddbf834b3de326d0ec8385c
chikoku <100092063+chikoku@users.noreply.github.com>
2021-07-27 20:51:30 +0000

    Fix travis deploy

--- src/dynamic_voca/DynamicVocaGUI.java
+++ src/dynamic_voca/DynamicVocaGUI.java
@@ -6,3 +6,6 @@ import java.awt.Component;
 import java.awt.Cursor;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Robot;
+import java.awt.event.ActionEvent;
