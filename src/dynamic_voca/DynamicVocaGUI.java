4a5c7950529071e15f8aec3c1addd78af9867989
chikoku <100092063+chikoku@users.noreply.github.com>
2021-07-27 21:07:14 +0000

    objects

--- src/dynamic_voca/DynamicVocaGUI.java
+++ src/dynamic_voca/DynamicVocaGUI.java
@@ -9,3 +9,7 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Robot;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.ItemEvent;
