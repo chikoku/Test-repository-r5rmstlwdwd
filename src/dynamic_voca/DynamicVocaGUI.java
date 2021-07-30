52b5e5087a96ca0eb3dc8bb8b3f4157c937198fe
chikoku <100092063+chikoku@users.noreply.github.com>
2021-07-30 06:17:48 +0000

    Trying to fix Travis

--- src/dynamic_voca/DynamicVocaGUI.java
+++ src/dynamic_voca/DynamicVocaGUI.java
@@ -25,3 +25,6 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
