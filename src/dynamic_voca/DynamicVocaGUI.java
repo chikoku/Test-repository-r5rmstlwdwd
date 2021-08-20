9c531a624ad20f291fa6b3dafba1dc3217d5d5ab
chikoku <100092063+chikoku@users.noreply.github.com>
2021-08-20 13:39:02 +0000

    optimizing

--- src/dynamic_voca/DynamicVocaGUI.java
+++ src/dynamic_voca/DynamicVocaGUI.java
@@ -281,3 +281,7 @@ public class DynamicVocaGUI extends JFrame {
 				Sound EnteredButtonSound = new Sound("ButtonEnteredSound.mp3", false);
 				EnteredButtonSound.start();
 			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				apndMenuButton.setIcon(apndMenuButtonImage);
