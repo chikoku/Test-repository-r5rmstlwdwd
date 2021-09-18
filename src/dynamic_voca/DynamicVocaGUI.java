0bc7e3236a9c623b8f55bbb480c196b258fa45e9
chikoku <100092063+chikoku@users.noreply.github.com>
2021-09-18 12:32:22 +0000

    travis: update

--- src/dynamic_voca/DynamicVocaGUI.java
+++ src/dynamic_voca/DynamicVocaGUI.java
@@ -595,3 +595,7 @@ public class DynamicVocaGUI extends JFrame {
 				cancelTestButton.setIcon(backEnteredButtonImage);
 				Sound EnteredButtonSound = new Sound("ButtonEnteredSound.mp3", false);
 				EnteredButtonSound.start();
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
