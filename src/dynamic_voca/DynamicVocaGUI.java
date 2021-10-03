9aa9f8310b7e664a61a2e1db1de8a93da3ca670f
chikoku <100092063+chikoku@users.noreply.github.com>
2021-10-03 08:50:43 +0000

    Clean logs

--- src/dynamic_voca/DynamicVocaGUI.java
+++ src/dynamic_voca/DynamicVocaGUI.java
@@ -791,3 +791,9 @@ public class DynamicVocaGUI extends JFrame {
 				Sound EnteredButtonSound = new Sound("ButtonEnteredSound.mp3", false);
 				EnteredButtonSound.start();
 			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				deleteLoadButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+				deleteLoadButton.setIcon(deleteButtonImage);
+			}
