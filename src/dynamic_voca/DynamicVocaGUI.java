e5984f16960cc23f647a6f932f697b37ca270c6d
chikoku <100092063+chikoku@users.noreply.github.com>
2021-11-23 21:37:33 +0000

    idea files

--- src/dynamic_voca/DynamicVocaGUI.java
+++ src/dynamic_voca/DynamicVocaGUI.java
@@ -1403,3 +1403,6 @@ public class DynamicVocaGUI extends JFrame {
 			@Override
 			public void mouseEntered(MouseEvent e) {
 				infoQuitButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+				infoQuitButton.setIcon(backEnteredButtonImage);
+				Sound EnteredButtonSound = new Sound("ButtonEnteredSound.mp3", false);
+				EnteredButtonSound.start();
