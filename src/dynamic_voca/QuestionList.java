c704d28d82c33027ea80ad8adb9ad3855b04beb3
chikoku <100092063+chikoku@users.noreply.github.com>
2022-03-14 06:34:26 +0000

    Added a file remotely

--- src/dynamic_voca/QuestionList.java
+++ src/dynamic_voca/QuestionList.java
@@ -7,4 +7,4 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.Set;
 
-public class QuestionList implements Serializa
+public class QuestionList implements Serializable {
