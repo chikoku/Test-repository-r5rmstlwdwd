db006df1d1ea24f78e68856151ae7b54bf2a5099
chikoku <100092063+chikoku@users.noreply.github.com>
2022-03-19 20:14:30 +0000

    config test

--- src/dynamic_voca/QuestionList.java
+++ src/dynamic_voca/QuestionList.java
@@ -8,4 +8,4 @@ import java.util.Iterator;
 import java.util.Set;
 
 public class QuestionList implements Serializable {
-	private static final long serialVersionUID = 2932603788418
+	private static final long serialVersionUID = 29326037884185078L;
