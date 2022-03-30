9a68544ebdd91c0355802558e0020838bcdb3246
chikoku <100092063+chikoku@users.noreply.github.com>
2022-03-30 16:40:05 +0000

    update with html5 compliant code

--- src/dynamic_voca/QuestionList.java
+++ src/dynamic_voca/QuestionList.java
@@ -14,4 +14,4 @@ public class QuestionList implements Serializable {
 	private ArrayList<Voca> reviewVocas;
 	private int questionNum;
 	private int correctCnt;
-	private int inco
+	private int incorrect
