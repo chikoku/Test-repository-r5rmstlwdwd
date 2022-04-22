af7bd3f0b0efbe9b8aebd83861b3ebfd2416f3b0
chikoku <100092063+chikoku@users.noreply.github.com>
2022-04-22 21:52:01 +0000

    Keratoconus

--- src/dynamic_voca/QuestionList.java
+++ src/dynamic_voca/QuestionList.java
@@ -27,4 +27,4 @@ public class QuestionList implements Serializable {
 	public void setQuestionVocas(Set<Voca> vocaSet) {
 		Iterator<Voca> itr = vocaSet.iterator();
 		while (itr.hasNext())
-			questionVocas.add(itr.next
+			questionVocas.add(itr.next());
