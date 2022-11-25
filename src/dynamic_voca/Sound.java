ff0013762665f29202c9975007e0774695970e32
chikoku <100092063+chikoku@users.noreply.github.com>
2022-11-25 06:51:38 +0000

    ceph: avoid 32-bit page index overflow

--- src/dynamic_voca/Sound.java
+++ src/dynamic_voca/Sound.java
@@ -4,3 +4,7 @@ package dynamic_voca;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+
+import javazoom.jl.player.Player;
+
+public class Sound extends Thread {
