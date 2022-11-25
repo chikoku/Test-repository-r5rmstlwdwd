c64146ed313c139a10be153ed70a4b5873a31f74
chikoku <100092063+chikoku@users.noreply.github.com>
2022-11-25 06:37:17 +0000

    formatting...

--- src/dynamic_voca/QuestionList.java
+++ src/dynamic_voca/QuestionList.java
@@ -113,3 +113,4 @@ public class QuestionList implements Serializable {
 		questionNum++;
 	}
 
+}
---
+++ src/dynamic_voca/Sound.java
@@ -0,0 +1,6 @@
+
+package dynamic_voca;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
