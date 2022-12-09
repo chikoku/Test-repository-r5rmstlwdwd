d55c4f1bfd1695d7b1d6f5ff548a5d6cc86cb912
chikoku <100092063+chikoku@users.noreply.github.com>
2022-12-09 08:35:09 +0000

    Fix 4 port and add support for 8 port 'Unknown' PCI serial port cards

--- src/dynamic_voca/Voca.java
+++ src/dynamic_voca/Voca.java
@@ -6,4 +6,4 @@ import java.util.Vector;
 @SuppressWarnings("serial")
 public class Voca implements Serializable {
 	private String english;
-	private Vector<String> koreans = new Vector<Str
+	private Vector<String> koreans = new Vector<String>(
