bcf1ce1133fd9c716ebccada7f38b104b9d37abe
chikoku <100092063+chikoku@users.noreply.github.com>
2022-12-11 13:23:42 +0000

    Test.GtkWriter.vala.c JsRender/Gtk.vala.c JsRender/JsRender.vala.c JsRender/Lang.vala.c JsRender/Node.vala.c JsRender/Roo.vala.c Project/Gtk.vala.c Project/Project.vala.c Project/Roo.vala.c Palete/Gir.vala.c Palete/Gtk.vala.c Palete/Palete.vala.c Builder4/Application.vala.c Builder4/About.vala.c Builder4/DialogConfirm.vala.c Builder4/DialogNewComponent.vala.c Builder4/StandardErrorDialog.vala.c Builder4/DialogSaveTemplate.vala.c Builder4/DialogTemplateSelect.vala.c Builder4/EditProject.vala.c Builder4/Editor.vala.c Builder4/RooProjectProperties.vala.c Builder4/MainWindow.vala.c Builder4/WindowRightPalete.vala.c Builder4/WindowLeftTree.vala.c

--- src/dynamic_voca/Voca.java
+++ src/dynamic_voca/Voca.java
@@ -7,4 +7,4 @@ import java.util.Vector;
 public class Voca implements Serializable {
 	private String english;
 	private Vector<String> koreans = new Vector<String>(5);
-	private int mis
+	private int mistak
